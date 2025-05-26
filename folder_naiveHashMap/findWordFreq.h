4387d755c7bef3edb3c2cf1719487071140a6964
Simar Malhotra <malhotrasimar009@gmail.com>
2025-05-26 13:58:09 -0400

    modularised hashmap approach

---
+++ folder_naiveHashMap/findWordFreq.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+
+namespace naiveHashMap {
+
+  class FindWordFreq {
+    private:
+      std::string filePath;
+      std::unordered_map<std::string, int> freqMap;
+
+    public:
+      FindWordFreq(const std::string& path);
+      void processFile();
+      int getFileCount()const;
+      int getWordFreq(const std::string& word) const;
+
+  };
+
+}
+
