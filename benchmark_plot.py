efa7557a7dbccfc9de50ec04ec403b51eddb82d8
Simar Malhotra <malhotrasimar009@gmail.com>
2025-05-28 16:00:10 -0400

    plot benchmark for 4 vs 8 vs 12 threads

--- benchmark_plot.py
+++ benchmark_plot.py
@@ -11,22 +11,29 @@ with open(bm_output, 'r') as f:
 
 benchmarks= benchmark_data['benchmarks']
 
-input_sizes_gb=[]
-times=[]
+thread_sizes = {
+    t: {'file_size': [], 'time': []}
+    for t in {b['name'].split('/')[2] for b in benchmarks}
+}
 
-threads= benchmarks[0]['name'].split('/')[2]
+for b in benchmarks:
+    t_size = b['name'].split('/')[2]
+    size_gb = int(b['name'].split('/')[1]) / (1024 ** 3)
+    time = b['real_time']
 
-for benchmark in benchmarks:
-    input_sizes_gb.append(int(benchmark['name'].split('/')[1]) / (1024 ** 3))
-    times.append(benchmark['real_time'])
+    thread_sizes[t_size]['file_size'].append(size_gb)
+    thread_sizes[t_size]['time'].append(time)
 
-    assert threads==benchmark['name'].split('/')[2]
+
+colors = ['dodgerblue', 'green', 'orange', 'crimson', 'purple', 'teal']
 
 plt.figure(figsize=(10, 6))
-plt.plot(input_sizes_gb, times, marker='o', linewidth=2, color='dodgerblue', label=f'{threads} Threads Internal')
-
-for x, y in zip(input_sizes_gb, times):
-    plt.text(x, y + 2, f"{y:.1f}s", ha='center', fontsize=9)
+for i, (t, values) in enumerate(sorted(thread_sizes.items(), key=lambda x: int(x[0]))):
+    color = colors[i % len(colors)]
+    plt.plot(values['file_size'], values['time'], marker='o', linewidth=2, color=color, label=f'{t} Threads')
+    
+    for x, y in zip(values['file_size'], values['time']):
+        plt.text(x, y + 2, f"{y:.1f}s", ha='center', fontsize=9)
 
 plt.title('📊 File Processing Benchmark: Runtime vs Input Size', fontsize=16, weight='bold')
 plt.xlabel('Input Size (GB)', fontsize=14)
@@ -38,4 +45,3 @@ plt.legend(fontsize=12, loc='upper left')
 plt.tight_layout()
 plt.savefig('benchmark.png')
 plt.show()
-
